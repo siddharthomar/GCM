4e51a00e877f89ce6bcb1aa9bf18639e2dbf7c76
Sidharth Omar <SO001122@IND.FONANTRIX.COM>
2017-01-18 16:52:18 +0530

    Initial Commit..

---
+++ app/src/main/java/com/fonantrix/demo/MainActivity.java
@@ -0,0 +1,101 @@
+package com.fonantrix.demo;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //defining views
+    private Button buttonSendPush;
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private ProgressDialog progressDialog;
+
+    //URL to RegisterDevice.php
+    private static final String URL_REGISTER_DEVICE = "http://crri.16mb.com/GCM/RegisterDevice.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getting views from xml
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        buttonSendPush = (Button) findViewById(R.id.buttonSendNotification);
+
+        //adding listener to view
+        buttonRegister.setOnClickListener(this);
+        buttonSendPush.setOnClickListener(this);
+    }
+
+    //storing token to mysql server
+    private void sendTokenToServer() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering Device...");
+        progressDialog.show();
+
+        final String token = SharedPrefManager.getInstance(this).getDeviceToken();
+        final String email = editTextEmail.getText().toString();
+
+        if (token == null) {
+            progressDialog.dismiss();
+            Toast.makeText(this, "Token not generated", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGISTER_DEVICE,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this,response, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("token", token);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonRegister) {
+            sendTokenToServer();
+        }
+        if(view == buttonSendPush){
+            startActivity(new Intent(this, ActivitySendPushNotification.class));
+
+
+        }
+    }
+}
